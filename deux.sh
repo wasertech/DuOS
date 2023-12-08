4970159704c664c002d53d8ed96b6ce8bdb7e485
wasertech <danny@waser.tech>
2023-12-08 01:55:59 +0100

    Fix user creation commands

--- deux.sh
+++ deux.sh
@@ -29,13 +29,13 @@ function init() {
     echo "User creation"
     # create user interactively w/ administrative privileges
     read -p "Enter the username of the user to create: " DUO_USER
-    login $DUO_DISTRO -- -c useradd -m -G wheel -s /bin/bash $DUO_USER
-    login $DUO_DISTRO -- -c passwd $DUO_USER
+    login $DUO_DISTRO -- useradd -m -G wheel -s /bin/bash $DUO_USER
+    login $DUO_DISTRO -- passwd $DUO_USER
 
-    login $DUO_DISTRO -- -c echo "export DUO_USER=$DUO_USER" >> ~/.bashrc
-    login $DUO_DISTRO -- -c echo "source deux.sh" >> ~/.bashrc
-    login $DUO_DISTRO -- -c echo "export DUO_USER=$DUO_USER" >> ~/.zshrc
-    login $DUO_DISTRO -- -c echo "source deux.zsh" >> ~/.zshrc
+    login $DUO_DISTRO -- echo "export DUO_USER=$DUO_USER" >> ~/.bashrc
+    login $DUO_DISTRO -- echo "source deux.sh" >> ~/.bashrc
+    login $DUO_DISTRO -- echo "export DUO_USER=$DUO_USER" >> ~/.zshrc
+    login $DUO_DISTRO -- echo "source deux.zsh" >> ~/.zshrc
     echo "User created successfully."
 }
 
